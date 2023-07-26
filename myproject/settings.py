5f85b005b12eb7378b4519e412c8020ef3dcf707
Thammarat Noo-ngoen <74042406+tOnzm@users.noreply.github.com>
2023-07-26 23:31:24 +0700

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g*0xtq2^brsaz4n@h+h8%xv(g&zw2g&%tnzn_w)8#hkj-obxao'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = false 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [https://tonzm.github.io/myproject_tat_django/]
 
 
 # Application definition
