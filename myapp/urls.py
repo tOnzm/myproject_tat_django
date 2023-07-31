21b7de8b22d818cd4070b1a4c935f03cee1d2d85
Thammarat Noo-ngoen <tonselaman@gmail.com>
2023-08-01 04:16:17 +0700

    t

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from myapp import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('',views.index),
     path('about',views.about),
     path('edit/<person_id>',views.edit),
     path('delete/<person_id>',views.delete),
+    
+
 
    
 ]
--- myproject/settings.py
+++ myproject/settings.py
@@ -25,17 +25,18 @@ SECRET_KEY = 'django-insecure-g*0xtq2^brsaz4n@h+h8%xv(g&zw2g&%tnzn_w)8#hkj-obxao
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '127.0.0.1',]
 
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = [    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'livereload',
     'django.contrib.staticfiles',
     'django_bootstrap_icons',
     'myapp',
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'myproject.urls'
@@ -117,13 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_URL += 'images/'
-STATIC_URL += 'css/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-
+STATIC_URL = '/static/'
+""" STATIC_ROOT = BASE_DIR / 'assets' """
+STATICFILES_DIRS = [
+    BASE_DIR / 'statics',
+] 
 
 
 # Default primary key field type
